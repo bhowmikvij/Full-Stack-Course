e90a21e4d92cfcbff30a8c2d069718c2a6618d88
bhowmikvij <bhowmik.vij14@gmail.com>
2026-07-07 23:49:54 +0530

    JS Started Today

---
+++ JS/intro.js
@@ -0,0 +1,7 @@
+// console.log("Hello");
+// console.log("I like pizza");
+
+// window.alert(`This is an alert`) // This is the popup message which says this is an alert
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza`;
