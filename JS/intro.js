7e0ac298636c5332cbea9d1662a04e678fb748cf
bhowmikvij <bhowmik.vij14@gmail.com>
2026-07-09 17:19:17 +0530

    variables added

--- JS/intro.js
+++ JS/intro.js
@@ -4,4 +4,4 @@
 // window.alert(`This is an alert`) // This is the popup message which says this is an alert
 
 document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza`;
+document.getElementById("myP").textContent = `I like pizza`; 
---
+++ JS/variables.js
@@ -0,0 +1,43 @@
+// variables = a container that stores a value.
+//             Behaves as if it were the value it contains.
+
+// 1. declaration     Let x;
+// 2. assignment      x = 100;
+
+// --------------------------------------------------------------------
+
+// let x;
+// x = 123;
+
+// console.log(x);
+
+// -------------------------------------------------------------------
+
+// let age = 25;
+// let hours = 24;
+// let sgpa = 7.26;
+// let firstName = "Bhowmik"
+// let favoriteFood = "Non Veg"
+// let yourEmail = "notoriginal123email.com"
+
+
+// console.log(`Your are ${age} years old`);
+// console.log(`we have ${hours} in a day`);
+// console.log(`my sgpa ${sgpa} for this sem`);
+// console.log(`My first name is ${firstName}`);
+
+// -----------------------------------------------------------------
+
+// let online = true;
+// console.log(typeof online); // type of is used for checking datatypes 
+
+// -----------------------------------------------------------------
+
+let age = 25;
+let hours = 24;
+let sgpa = 7.26;
+
+document.getElementById("p1").textContent = `Your are ${age} years old`
+document.getElementById("p2").textContent = `we have ${hours} in a day`
+document.getElementById("p3").textContent = `my sgpa ${sgpa} for this sem`
+
